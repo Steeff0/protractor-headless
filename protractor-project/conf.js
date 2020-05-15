dd8806437fcd0cc35c15e812cea3990f0da7acb3
Steven Gerritsen <steven.gerritsen@gmail.com>
2020-05-15 13:19:55 +0200

    Add npminstall to entrypoint

--- protractor-project/conf.js
+++ protractor-project/conf.js
@@ -1,23 +1,67 @@
-
 exports.config = {
     framework: 'jasmine',
     specs: ['specs/*.specs.js'],
     multiCapabilities: [{
         browserName: 'chrome'
     }],
-    onPrepare: function(){
+    onPrepare: function () {
+        //Getting CLI report
         const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
         jasmine.getEnv().addReporter(new SpecReporter({
             spec: {
                 displayStacktrace: true
             }
         }));
+
         //configure junit xml report
         var jasmineReporters = require('jasmine-reporters');
         jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
             consolidateAll: true,
             filePrefix: 'guitest-xmloutput',
-            savePath: './'
+            savePath: './reports'
         }));
-    }
+
+        //Getting screenshots
+        var fs = require('fs-extra');
+        fs.emptyDir('./reports/screenshots/', function (err) {
+            console.log(err);
+        });
+        jasmine.getEnv().addReporter({
+            specDone: function (result) {
+                if (result.status == 'failed') {
+                    browser.getCapabilities().then(function (caps) {
+                        var browserName = caps.get('browserName');
+                        browser.takeScreenshot().then(function (png) {
+                            var stream = fs.createWriteStream('./reports/screenshots/' + browserName + '-' + result.fullName + '.png');
+                            stream.write(new Buffer.from(png, 'base64'));
+                            stream.end();
+                        });
+                    });
+                }
+            }
+        });
+    },
+    onComplete: function () {
+        //Getting HTML report
+        var browserName, browserVersion;
+        var capsPromise = browser.getCapabilities();
+        capsPromise.then(function (caps) {
+            browserName = caps.get('browserName');
+            browserVersion = caps.get('version');
+            platform = caps.get('platform');
+            var HTMLReport = require('protractor-html-reporter-2');
+            testConfig = {
+                reportTitle: 'Protractor Test Execution Report',
+                outputPath: './reports/html',
+                outputFilename: 'ProtractorTestReport',
+                screenshotPath: '../screenshots',
+                testBrowser: browserName,
+                browserVersion: browserVersion,
+                modifiedSuiteName: false,
+                screenshotsOnlyOnFailure: true,
+                testPlatform: platform
+            };
+            new HTMLReport().from('./reports/guitest-xmloutput.xml', testConfig);
+        });
+    },
 }
