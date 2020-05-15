e6f57ceb5d191d5f401b20b8da9b7cfc19aa574a
Steven Gerritsen <steven.gerritsen@gmail.com>
2020-05-15 12:51:29 +0200

    Add Reporting

--- protractor-project/conf.js
+++ protractor-project/conf.js
@@ -1,7 +1,23 @@
+
 exports.config = {
     framework: 'jasmine',
     specs: ['specs/*.specs.js'],
     multiCapabilities: [{
         browserName: 'chrome'
-    }]
+    }],
+    onPrepare: function(){
+        const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+        jasmine.getEnv().addReporter(new SpecReporter({
+            spec: {
+                displayStacktrace: true
+            }
+        }));
+        //configure junit xml report
+        var jasmineReporters = require('jasmine-reporters');
+        jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+            consolidateAll: true,
+            filePrefix: 'guitest-xmloutput',
+            savePath: './'
+        }));
+    }
 }
